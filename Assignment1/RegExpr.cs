7f49bdde830d8111a4d18d649ed785fb6f929a2b
kbekj <95280227+kbekj@users.noreply.github.com>
2022-09-14 16:36:51 +0200

    url regex

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -13,27 +13,25 @@ public static class RegExpr
         }
     }
 
-    public static IEnumerable<(int width, int height)> Resolution(string resolutions){
-        string match1 ="";
-        string match2 ="";
-        int i = 0;
-        foreach(Match match in Regex.Matches(resolutions, @"[0-9]+")){
-            var matchvalue = match.ToString();
-            if (i%2==0){
-                match1 = matchvalue;
-            }else{
-                match2 = matchvalue;
-                yield return (Int32.Parse(match1), Int32.Parse(match2));
+    public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions){
+        foreach(string line in resolutions){
+            foreach(Match match in Regex.Matches(line, @"(?<width>[0-9]+)x(?<height>[0-9]+)")){ 
+            yield return (Int32.Parse(match.Groups.GetValueOrDefault("width").ToString()), Int32.Parse(match.Groups.GetValueOrDefault("height").ToString()));
             }
-            i++;
         }
     }    
 
     public static IEnumerable<string> InnerText(string html, string tag){
-
-        foreach(Match match in Regex.Matches(html, @"([A-Za-z ]+)</"+ tag +@">")){
-            string innerText = match.Groups[1].ToString();
+        foreach(Match match in Regex.Matches(html, @"<(" + tag + @").*?>.+?</\1>")){
+            string innerText = Regex.Replace(match.ToString(), @"<.+?>", "");
             yield return innerText; 
+        }
+    }
+
+    public static IEnumerable<(Uri, string)> Urls(string html){
+        foreach(Match match in Regex.Matches(html, @"href=.([a-z:./A-Z_()]+). ?title=.([a-zA-Z ()]+).")){
+            var groups = match.Groups;
+            yield return (new Uri(groups[1].ToString()), groups[2].ToString());
         }
     }
 }
